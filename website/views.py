c72a025118236df56d4bf9e3598b88ff772c0f7e
Earnest Achayo <earnytechlive@gmail.com>
2025-03-09 20:10:41 +0300

    updates

--- website/views.py
+++ website/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+    # Check to see if user s logged in
+    return render(request, 'home.html', {})
+
+
+def logout_user(requeest):
+    pass
